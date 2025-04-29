b837a7231f08ebee3d3c8fb8a69d507cfe82b4ae
hodynkaa <katyahour@gmail.com>
2025-04-29 09:56:08 +0200

    Pierwszy zapis do repozytorium Lab8

---
+++ src/pl/pp/Konto.java
@@ -0,0 +1,82 @@
+package pl.pp;
+
+public class Konto {
+    private String numerKonta;     // numer konta
+    private double saldo;          // dostępne środki
+    private String wlasciciel;     // imię i nazwisko właściciela
+    private String email;          // adres e-mail właściciela
+    private String telefon;        // numer telefonu właściciela
+
+    public Konto() {
+    }
+
+    public Konto(String numerKonta, double saldo, String wlasciciel, String email, String telefon) {
+        this.numerKonta = numerKonta;
+        this.saldo = saldo;
+        this.wlasciciel = wlasciciel;
+        this.email = email;
+        this.telefon = telefon;
+    }
+
+    public void wplata(double kwota) {
+        if (kwota > 0) {
+            saldo += kwota;
+            System.out.println("Wpłata PLN " + kwota + " została wykonana. Nowe saldo: PLN " + saldo);
+        } else {
+            System.out.println("Kwota musi być większa niż 0.");
+        }
+    }
+
+    public void wyplata(double kwota) {
+        if (kwota <= saldo) {
+            saldo -= kwota;
+            System.out.println("Pobrano PLN " + kwota + " z konta. Pozostałe saldo: PLN " + saldo);
+        } else {
+            System.out.println("Brak środków. Masz tylko PLN " + saldo + " na koncie.");
+        }
+    }
+
+    public String getNumerKonta() {
+        return numerKonta;
+    }
+
+    public void setNumerKonta(String numerKonta) {
+        this.numerKonta = numerKonta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getWlasciciel() {
+        return wlasciciel;
+    }
+
+    public void setWlasciciel(String wlasciciel) {
+        this.wlasciciel = wlasciciel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+}
+
+
+
+
